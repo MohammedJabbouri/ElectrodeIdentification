5686cfe92c80487ea3c161f4660fe076f7298d17
MohammedJabbouri <mohammedbelmeharzia@gmail.com>
2026-04-20 07:24:28 -0700

    Update trigger.cpp

--- firmware/trigger.cpp
+++ firmware/trigger.cpp
@@ -1,1 +1,13 @@
+#include <Arduino.h>
+#include "config.hpp"
 
+void initTrigger() {
+    pinMode(PIN_TRIGGER, OUTPUT);
+    digitalWrite(PIN_TRIGGER, LOW);
+}
+
+void fireTrigger() {
+    digitalWrite(PIN_TRIGGER, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(PIN_TRIGGER, LOW);
+}
